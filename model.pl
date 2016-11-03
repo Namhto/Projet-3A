2550db3fe46f8ae23c4173844994a0cc5f49a3d4
Namhto <otenich@gmail.com>
2016-11-03 09:55:21 +0100

    FIRST COMMIT

---
+++ model.pl
@@ -0,0 +1,49 @@
+enseignant(perronne).
+enseignant(hassenforder).
+
+promotion('3A IR').
+promotion('2A AS').
+
+matiere('android').
+matiere('c++').
+matiere('java').
+
+tp('android').
+normal('c++').
+normal('java').
+
+salle('pc-master').
+salle('ga').
+salle('e23').
+
+machine('pc-master').
+salleNormale('ga').
+salleNormale('e23').
+
+creneau('8h_10h').
+creneau('10h_12h').
+
+cours('c++', perronne, '2A AS').
+cours('android', hassenforder, '3A IR').
+cours('java', perronne, '3A IR').
+
+tpEnMachine(Salle, Matiere) :-	salle(Salle),
+								machine(Salle),
+								tp(Matiere).
+								
+pasTP(Salle, Matiere) :-	salle(Salle),
+							salleNormale(Salle),
+							normal(Matiere).
+							
+							
+							
+							
+attribution(Salle, cours(Matiere,Enseignant,Promotion), Creneau) :- salle(Salle),
+																	cours(Matiere,Enseignant,Promotion),
+																	creneau(Creneau),
+																	tpEnMachine(Salle, Matiere).
+																	
+attribution(Salle, cours(Matiere,Enseignant,Promotion), Creneau) :- salle(Salle),
+																	cours(Matiere,Enseignant,Promotion),
+																	creneau(Creneau),
+																	pasTP(Salle, Matiere).
