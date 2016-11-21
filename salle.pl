507be064e515700fafed071fcaac37d83d9f8a23
Maxpal92 <maxime.marcia@yahoo.fr>
2016-11-21 10:23:51 +0100

    Fichier contenant les informations sur les salles

---
+++ salle.pl
@@ -0,0 +1,27 @@
+%% Ce fichier contient les informations relatives aux salles de cours &&
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% Les salles %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+room(GA).
+room(PA).
+room(e23).
+room(e25).
+room(PC Master).
+
+%% Taille des salles %%
+roomSize(R,S):- room(R),number(S).
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% Equipements %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+equipment(computer).
+equipment(motor).
+equipment(projector).
+equipment(board).
+
+roomEquipment(R,E):- room(R), equipment(E).
+
+%%Types de salles%%
+
+
