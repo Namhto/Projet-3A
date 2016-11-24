b4f243ed1973122b539b7f7dd603f3941b4f633e
Maxpal92 <maxime.marcia@yahoo.fr>
2016-11-24 11:01:26 +0100

    Séparation de fichier : teacher

---
+++ teacher.pl
@@ -0,0 +1,13 @@
+
+use_module('./course').
+%%%%%%%%%%%%%%%%
+%%%  Teacher %%%
+%%%%%%%%%%%%%%%%
+
+teacher(perrone).
+teacher(hassenforder).
+teacher(forestier).
+teacher(kutter).
+
+teaches(perrone,c).
+teaches(perrone,java).
