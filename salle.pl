d9ea96c628ffa35a13909bc056997272ef89672d
Namhto <otenich@gmail.com>
2016-11-24 10:46:29 +0100

    correction code définition de module

--- salle.pl
+++ salle.pl
@@ -1,29 +1,42 @@
+:-module(salle,[room/1, roomSize/2, roomEquipment/2, roomType/2])
+
 %% Ce fichier contient les informations relatives aux salles de cours &&
 
+%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%% Salles %%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%
-%%% Les salles %%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%
-room(GA).
-room(PA).
-room(e23).
-room(e25).
-room(PC Master).
+room('GA').
+room('PA').
+room('e23').
+room('e25').
+room('PC Master').
 
-%% Taille des salles %%
-roomSize(R,S):- room(R),number(S).
+%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%% Taille %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+roomSize('GA', 80).
+roomSize('PA', 60).
+roomSize('e23', 40).
+roomSize('e25', 50).
+roomSize('PC Master', 25).
 
 %%%%%%%%%%%%%%%%%%%%%%
 %%% Equipements %%%%%%
 %%%%%%%%%%%%%%%%%%%%%%
-equipment(computer).
-equipment(motor).
-equipment(projector).
-equipment(board).
+equipment('computer').
+equipment('motor').
+equipment('projector').
+equipment('board').
 
 roomEquipment(R,E):- room(R), equipment(E).
 
-%%Types de salles%%
-roomType(TP).
-roomType(normal).
+%%%%%%%%%%%%%%%%%%%%%%
+%%%% Type salle %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+roomType('GA', normal).
+roomType('PA', normal).
+roomType('e23', normal).
+roomType('e25', normal).
+roomType('PC Master', TP).
 
 
