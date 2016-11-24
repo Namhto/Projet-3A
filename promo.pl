77bba9f218be1dd2bea37a64d88d332f9bb3ee6f
Namhto <otenich@gmail.com>
2016-11-24 10:18:31 +0100

    Ajout groupes de TD et TP à promo.pl

--- promo.pl
+++ promo.pl
@@ -1,44 +1,65 @@
 :-module(promotion,[promotion/1],[group/2],[branch/1])
 
-%% Ce fichier contient la liste des promotions. &&
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%% Les promotions %%%
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Les promotions %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 promotion('1A').
 promotion('2A').
 promotion('3A').
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%% Les Filières %%%%%
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%% Les Filières %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 branch('IR').
 branch('AS').
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%% Les Groupes %%%%%%
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Les Groupes de TD %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-group('1A', 'IR', g1).
-group('1A', 'IR', g2).
-group('2A', 'IR', g3).
-group('2A', 'IR', g4).
-group('3A', 'IR', g5).
-group('3A', 'IR', g6).
+groupTD('1A', 'IR', g1).
+groupTD('1A', 'IR', g2).
+groupTD('2A', 'IR', g3).
+groupTD('2A', 'IR', g4).
+groupTD('3A', 'IR', g5).
+groupTD('3A', 'IR', g6).
 
-group('1A', 'AS', g7).
-group('1A', 'AS', g8).
-group('2A', 'AS', g9).
-group('2A', 'AS', g10).
-group('3A', 'AS', g11.
-group('3A', 'AS', g12).
+groupTD('1A', 'AS', g7).
+groupTD('1A', 'AS', g8).
+groupTD('2A', 'AS', g9).
+groupTD('2A', 'AS', g10).
+groupTD('3A', 'AS', g11.
+groupTD('3A', 'AS', g12).
 
-group(Promo,Branch,GroupeName):- promotion(Promo),branch(Branch).
+groupTD(Promo,Branch,GroupeName):- promotion(Promo),branch(Branch).
 
-group(Name):-group(_,_,Name).
+groupTD(Name):-groupTD(_,_,Name).
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%% Contraintes %%%%%%
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Les Groupes de TD %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+groupTP('1A', 'IR', g1).
+groupTP('1A', 'IR', g2).
+groupTP('2A', 'IR', g3).
+groupTP('2A', 'IR', g4).
+groupTP('3A', 'IR', g5).
+groupTP('3A', 'IR', g6).
+
+groupTP('1A', 'AS', g7).
+groupTP('1A', 'AS', g8).
+groupTP('2A', 'AS', g9).
+groupTP('2A', 'AS', g10).
+groupTP('3A', 'AS', g11.
+groupTP('3A', 'AS', g12).
+
+groupTP(Promo,Branch,GroupeName):- promotion(Promo),branch(Branch).
+
+groupTP(Name):-groupTP(_,_,Name).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% Contraintes %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
