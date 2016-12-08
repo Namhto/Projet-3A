694d80222dc8221216dcbf8e2ad924c28bc4c40c
Namhto <otenich@gmail.com>
2016-12-08 10:09:57 +0100

    contraine salle / promo / prof uniques sur un créneau

--- main.pl
+++ main.pl
@@ -20,23 +20,45 @@ learn('java', section('2A','IR')).
 time(height).
 time(ten).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%% Cours magistral %%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-courseInstance(Teacher, Promotion, Place, CourseLabel, Time) :- isMagistral(CourseLabel), teaches(Teacher,CourseLabel), learn(CourseLabel, section(Promotion, Branch)), room(Place), time(Time).
+courseInstance(Teacher, Student, Place, CourseLabel, Time) :- teaches(Teacher,CourseLabel), groupTD(Student), learn(CourseLabel, Student), room(Place), time(Time).
 
-instanciate(courseInstance(T,Promotion, B,P,C,Time)) :- courseInstance(T,Promotion, B,P,C,Time). 
+instanciate(courseInstance(T,S,P,C,Time)) :- courseInstance(T,S,P,C,Time). 
 
-schedule(S) :- instanciate(C1), append([],[C1],S1), 
-				instanciate(C2), append(S1,[C2],S).
+scheduleAuto(S,X) :- schedule_Acc([], S, X).
 
-uniqueRessource([_]) :- write('alalala').
-uniqueRessource([courseInstance(_,_,_,P,_,Time)|R]) :- member(courseInstance(_,_,_,P,_,Time),R), !, fail.
-uniqueRessource([courseInstance(T,_,_,_,_,Time)|R]) :- member(courseInstance(T,_,_,_,_,Time),R), !, fail.
+schedule_Acc(S,S,0) :- !.
+schedule_Acc(L,S,X) :- instanciate(C), append(L,[C],L1),
+						Y is X-1,
+						schedule_Acc(L1,S,Y).
+						
+schedule(S) :- nbrCourses(X), scheduleAuto(S,X), uniqueRessource(S).
+
+nbrCourses(X) :- findall(P,learn(P,_),L), length(L,X).						
+
+
+uniqueRessource([]).
+uniqueRessource([_]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%% Contrainte de salle : salle utilisée une seule fois sur un créneau %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+uniqueRessource([courseInstance(_,_,P,_,Time)|R]) :- member(courseInstance(_,_,P,_,Time),R), !, fail.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% Contrainte de prof : prof a une seule fois cours sur un créneau %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+uniqueRessource([courseInstance(T,_,_,_,Time)|R]) :- member(courseInstance(T,_,_,_,Time),R), !, fail.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% Contrainte de promo : promo a une seule fois cours sur un créneau %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+uniqueRessource([courseInstance(_,S,_,_,Time)|R]) :- member(courseInstance(_,S,_,_,Time),R), !, fail.
+
 uniqueRessource([_|R]) :- uniqueRessource(R).
 
-/**
 pascourta8h([]).
 pascourta8h([courseInstance(perrone,_,_,_,height)|_R]) :- !, fail.
 pascourta8h([_|R]) :-  pascourta8h(R).
-*/
